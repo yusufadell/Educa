e13bac961858916b32d6d0a3653f919f9cd22e77
yusufadell <youseefadel777@gmail.com>
2022-02-16 12:55:34 +0200

    Remove Useless Comments

--- educa/educa/urls.py
+++ educa/educa/urls.py
@@ -4,6 +4,8 @@ from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
 from courses.views import CourseListView
+from rest_framework.schemas import get_schema_view
+
 
 urlpatterns = [
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
@@ -13,6 +15,11 @@ urlpatterns = [
     path('', CourseListView.as_view(), name='course_list'),
     path('students/', include('students.urls')),
     path('api/', include('courses.api.urls', namespace='api')),
+    path('openapi', get_schema_view(  # new
+        title="Educa API",
+        description="Api For Enrolling in Courses.",
+        version="1.0.0"
+    ), name='openapi-schema'),
 ]
 
 if settings.DEBUG:
