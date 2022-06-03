afbee42524a60070d9a90a41430003ee98c3f545
Restyled.io <commits@restyled.io>
2022-06-03 22:08:21 +0000

    Restyled by reorder-python-imports

--- educa/routing.py
+++ educa/routing.py
@@ -1,5 +1,6 @@
 from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.routing import ProtocolTypeRouter
+from channels.routing import URLRouter
 
 import chat.routing
 
