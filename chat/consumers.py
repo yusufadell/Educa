da1f6c8652450c3f313c4eaf50fe7bb1573dd7be
Restyled.io <commits@restyled.io>
2022-06-18 02:50:52 +0000

    Restyled by black

--- chat/consumers.py
+++ chat/consumers.py
@@ -5,40 +5,33 @@ from django.utils import timezone
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
-
     async def connect(self):
-        self.id = self.scope['url_route']['kwargs']['course_id']
-        self.user = self.scope['user']
-        self.room_group_name = 'chat_%s' % self.id
+        self.id = self.scope["url_route"]["kwargs"]["course_id"]
+        self.user = self.scope["user"]
+        self.room_group_name = "chat_%s" % self.id
         # join room group
-        await self.channel_layer.group_add(
-            self.room_group_name,
-            self.channel_name
-        )
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
         await self.accept()
 
     async def disconnect(self, close_code):
         # leave room group
-        await self.channel_layer.group_discard(
-            self.room_group_name,
-            self.channel_name
-        )
+        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
     # Receive message from WebSocket
     # receive message from WebSocket
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        message = text_data_json['message']
+        message = text_data_json["message"]
         now = timezone.now()
         # send message to room group
         await self.channel_layer.group_senda(
             self.room_group_name,
             {
-                'type': 'chat_message',
-                'message': message,
-                'user': self.user.username,
-                'time': now.strftime("%H:%M:%S")
-            }
+                "type": "chat_message",
+                "message": message,
+                "user": self.user.username,
+                "time": now.strftime("%H:%M:%S"),
+            },
         )
 
     # receive message from room group
--- educa/settings.py
+++ educa/settings.py
@@ -61,21 +61,27 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "educa.urls"
 CHANNEL_LAYERS = {
-    "default": {"BACKEND": "channels_redis.core.RedisChannelLayer", "CONFIG": {"hosts": ["redis://localhost:6379/0"]}}}
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {"hosts": ["redis://localhost:6379/0"]},
+    }
+}
 
-TEMPLATES = [{
-    "BACKEND": "django.template.backends.django.DjangoTemplates",
-    "DIRS": [],
-    "APP_DIRS": True,
-    "OPTIONS": {
-        "context_processors": [
-            "django.template.context_processors.debug",
-            "django.template.context_processors.request",
-            "django.contrib.auth.context_processors.auth",
-            "django.contrib.messages.context_processors.messages",
-        ]
-    },
-}]
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ]
+        },
+    }
+]
 
 WSGI_APPLICATION = "educa.wsgi.application"
 
@@ -94,22 +100,13 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME":
-            ("django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
-             )
+        "NAME": (
+            "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
+        )
     },
-    {
-        "NAME":
-            "django.contrib.auth.password_validation.MinimumLengthValidator"
-    },
-    {
-        "NAME":
-            "django.contrib.auth.password_validation.CommonPasswordValidator"
-    },
-    {
-        "NAME":
-            "django.contrib.auth.password_validation.NumericPasswordValidator"
-    },
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
 
 # Internationalization
@@ -149,8 +146,9 @@ CACHE_MIDDLEWARE_SECONDS = 60 * 15  # 15 minutes
 CACHE_MIDDLEWARE_KEY_PREFIX = "educa"
 
 REST_FRAMEWORK = {
-    "DEFAULT_PERMISSION_CLASSES":
-        ["rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"]
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
+    ]
 }
 
 # define the main ASGI application
