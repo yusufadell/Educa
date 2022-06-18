660841239b9f22f80fe3ee1769cb5e0a8d5c0382
Restyled.io <commits@restyled.io>
2022-06-18 02:50:54 +0000

    Restyled by reorder-python-imports

--- students/views.py
+++ students/views.py
@@ -1,14 +1,15 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate
+from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, FormView
+from django.views.generic.edit import CreateView
+from django.views.generic.edit import FormView
 from django.views.generic.list import ListView
 
-from courses.models import Course
-
 from .forms import CourseEnrollForm
+from courses.models import Course
 
 
 class StudentRegistrationView(CreateView):
