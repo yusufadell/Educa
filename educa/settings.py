416d436a66245a4872465984d2f8c2098e21af6a
yusufadell <youssef.31057404@ics.tanta.edu.eg>
2022-06-18 04:50:02 +0200

    🌟 create a chat server using Channels
     - implemented a WebSocket consumer and client.
     -  enabled communication between consumers using a channel layer with Redis
    
     - modified the consumer to be fully asynchronous

--- educa/settings.py
+++ educa/settings.py
@@ -60,6 +60,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = "educa.urls"
+CHANNEL_LAYERS = {
+    "default": {"BACKEND": "channels_redis.core.RedisChannelLayer", "CONFIG": {"hosts": ["redis://localhost:6379/0"]}}}
 
 TEMPLATES = [{
     "BACKEND": "django.template.backends.django.DjangoTemplates",
@@ -93,20 +95,20 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME":
-        ("django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
-         )
+            ("django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
+             )
     },
     {
         "NAME":
-        "django.contrib.auth.password_validation.MinimumLengthValidator"
+            "django.contrib.auth.password_validation.MinimumLengthValidator"
     },
     {
         "NAME":
-        "django.contrib.auth.password_validation.CommonPasswordValidator"
+            "django.contrib.auth.password_validation.CommonPasswordValidator"
     },
     {
         "NAME":
-        "django.contrib.auth.password_validation.NumericPasswordValidator"
+            "django.contrib.auth.password_validation.NumericPasswordValidator"
     },
 ]
 
@@ -148,7 +150,7 @@ CACHE_MIDDLEWARE_KEY_PREFIX = "educa"
 
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES":
-    ["rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"]
+        ["rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"]
 }
 
 # define the main ASGI application
